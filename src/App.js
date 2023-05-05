1bcba0d508fe346feefb20e6d413e39ade672c28
FrodeBerg <bergsetdt.frode@gmail.com>
2023-05-05 19:42:45 +0200

    Add Size setting to canvas

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import {React, useState} from 'react';
 
 
 function App() {
+  const [size, setSize] = useState({"x" : 0, "y" : 0})
+  
+  function setSizeFunction(indicator, value) {
+    setSize({
+      ...size,
+      [indicator] : +value
+    })
+  }
+
   return (
     <div className="App">
-      <Main /> 
+      <Main size = {size}/> 
       <Side /> 
-      <Bottom />
+      <Bottom size={size} setSize={setSizeFunction}/>
     </div>
   );
 }
 
-function Main() {
+function Main(props) {
   const [posY, setPosY] = useState(0)
   const [posX, setPosX] = useState(0)
 
@@ -28,6 +37,7 @@ function Main() {
     <div className="Main"> 
       <canvas id="canvas"></canvas> 
       <h3>Y: {posY}, X: {posX}</h3>
+      <h3>SizeX: {props.size.x}, SizeY: {props.size.y}</h3>
     </div>
   )
 }
@@ -42,11 +52,12 @@ function Side() {
   )
 }
 
-function Bottom() {
+function Bottom(props) {
+
   return (
     <div className="Bottom"> 
-      <h2>x : 21</h2>
-      <h2>y : 12</h2>
+      <input label={props.size[0]} onChange={(e) => {props.setSize("x", (e.target.value))}}/>
+      <input label={props.size[1]} onChange={(e) => {props.setSize("y", (e.target.value))}}/>
       <h2>scale : 1</h2>
     </div>
   )
