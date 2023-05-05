d62e54828eac7039b9abbdece39c7dc973c762a2
FrodeBerg <bergsetdt.frode@gmail.com>
2023-05-05 17:25:38 +0200

    Add basic grid layout

--- src/App.js
+++ src/App.js
@@ -3,8 +3,39 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <Main /> 
+      <Side /> 
+      <Bottom />
     </div>
   );
+}
+
+function Main() {
+  return (
+    <div className="Main"> 
+      <canvas></canvas>
+    </div>
+  )
+}
+
+function Side() {
+  return (
+    <div className="Side"> 
+      <h2>Canvas</h2>
+      <h2>Settings</h2>
+      <h2>Play</h2>
+    </div>
+  )
+}
+
+function Bottom() {
+  return (
+    <div className="Bottom"> 
+      <h2>x : 21</h2>
+      <h2>y : 12</h2>
+      <h2>scale : 1</h2>
+    </div>
+  )
 }
 
 export default App;
