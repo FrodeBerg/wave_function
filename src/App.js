ae7ce7e25c3e1928721a8e79e16c2f9bf9a4ed98
FrodeBerg <bergsetdt.frode@gmail.com>
2023-05-08 17:48:37 +0200

    Move canvas functions into Canvas

--- src/App.js
+++ src/App.js
@@ -7,24 +7,32 @@ document.onmousedown = () => isPressed = true
 document.onmouseup = () => isPressed = false
 
 function App() {
-  const [size, setSize] = useState({"x" : 10, "y" : 10})
-  const [brushSize, setBrushSize] = useState(1)
-  const [color, setColor] = useState("#000000")
+
+  let Main = <Canvas />
   return (
     <div className="App">
-      <Main size = {size} brushSize={brushSize} color={color}/> 
       <Side /> 
-      <Bottom size={size} setSize={setSize} 
-      brushSize={brushSize} setBrushSize={setBrushSize}
-      color={color} setColor={setColor}
-      />
+      {Main}
     </div>
   );
 }
 
-function Main(props) {
+function Side() {
+  return (
+    <div className="Side"> 
+      <h2>Canvas</h2>
+      <h2>Rules</h2>
+      <h2>Play</h2>
+    </div>
+  )
+}
 
-  let ratio = props.size.x / props.size.y
+function Canvas(props) {
+  const [size, setSize] = useState({"x" : 10, "y" : 10})
+  const [brushSize, setBrushSize] = useState(1)
+  const [color, setColor] = useState("#000000")
+
+  let ratio = size.x / size.y
   let height = 400
   let width = 400 * ratio;
   
@@ -32,54 +40,40 @@ function Main(props) {
     width = 800
     height = 800 / ratio
   }
-  console.log(height)
-  return (
-    <div className="Main"> 
-      <canvas id="canvas" 
-      onMouseDown={(e) => {move(e, props.brushSize, props.color)}} 
-      onMouseMove={(e) => {if (isPressed) move(e, props.brushSize, props.color)}} 
-      width={props.size.x} 
-      height={props.size.y}
-      style={{"width": width,"height" : height}}>
-      </canvas>
-    </div>
-  )
-}
 
-function move(e, size, color) {
-  const canvas = document.getElementById("canvas")    
-  if ( e.target.id !== "canvas") return
-
-  let rect = e.target.getBoundingClientRect();
-
-  let posX = Math.floor((e.clientX - rect.left) / canvas.offsetWidth * canvas.width);
-  let posY = Math.floor((e.clientY - rect.top) / canvas.offsetHeight * canvas.height);
-  let ctx = canvas.getContext("2d");
-  ctx.fillStyle = color;
-  ctx.fillRect(posX - Math.floor(size / 2), posY - Math.floor(size / 2), size, size)   
-  // console.log(ctx.getImageData(0,0, canvas.width, canvas.height))   
-}
-
-function Side() {
-  return (
-    <div className="Side"> 
-      <h2>Canvas</h2>
-      <h2>Settings</h2>
-      <h2>Play</h2>
-    </div>
-  )
-}
-
-function Bottom(props) {
+  function paint(e) {
+    const canvas = document.getElementById("canvas")    
+    if (e.target.id !== "canvas") return
+  
+    let rect = e.target.getBoundingClientRect();
+  
+    let posX = Math.floor((e.clientX - rect.left) / canvas.offsetWidth * canvas.width);
+    let posY = Math.floor((e.clientY - rect.top) / canvas.offsetHeight * canvas.height);
+    let ctx = canvas.getContext("2d");
+    ctx.fillStyle = color;
+    ctx.fillRect(posX - Math.floor(brushSize / 2), posY - Math.floor(brushSize/ 2), brushSize, brushSize)   
+    // console.log(ctx.getImageData(0,0, canvas.width, canvas.height))   
+  }
 
   return (
-    <div className="Bottom"> 
-      <input placeholder={10} onChange={(e) => {props.setSize({...props.size, "x" : +e.target.value})}}/>
-      <input placeholder={10} onChange={(e) => {props.setSize({...props.size, "y" : +e.target.value})}}/>
-      <input placeholder={1} onChange={(e) => {props.setBrushSize(+e.target.value)}}/>
-      <input type={"color"} placeholder={"#000000"} onChange={(e) => {props.setColor(e.target.value)}}/>
+    <div className="Main">
+      <div className='canvasContainer'> 
+        <canvas id="canvas" 
+        onMouseDown={(e) => {paint(e)}} 
+        onMouseMove={(e) => {if (isPressed) paint(e)}} 
+        width={size.x} 
+        height={size.y}
+        style={{"width": width,"height" : height}}>
+        </canvas>
+      </div>
+      <div className="Bottom"> 
+        <input placeholder={10} onChange={(e) => {setSize({...size, "x" : +e.target.value})}}/>
+        <input placeholder={10} onChange={(e) => {setSize({...size, "y" : +e.target.value})}}/>
+        <input placeholder={1} onChange={(e) => {setBrushSize(+e.target.value)}}/>
+        <input type={"color"} placeholder={"#000000"} onChange={(e) => {setColor(e.target.value)}}/>
+      </div>       
     </div>
-  )
+  )  
 }
 
 export default App;
